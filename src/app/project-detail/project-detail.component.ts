07a715fe679713e4dc99f7fed6405c9312224623
Cory West-Forbes and Ted Leary <ted@>
2018-05-09 15:08:28 -0700

    add donation bar and associated functions

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 import { Project } from '../model/project.model';
 import { ProjectService } from '../project.service';
 
+
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
@@ -13,6 +14,34 @@ import { ProjectService } from '../project.service';
 export class ProjectDetailComponent implements OnInit {
   projectId: number;
   projectToDisplay: Project;
+
+
+  addCommas(number) {
+    let string = number.toString();
+    let array = [];
+    let extra = '';
+    if (string.length < 3) {
+      return string;
+    }
+    if (string.length % 3 === 1) {
+      extra = string.slice(0, 1);
+    } else if (string.length % 3 === 2) {
+      extra = string.slice(0, 2);
+    }
+    for (var i = (string.length - 3) ; i >= 0; i-=3) {
+      array.unshift(string.slice(i, 3 + i));
+    }
+    if (extra) {
+      return extra + ',' + array.join(',');
+    }
+    return array.join(',');
+  }
+
+
+  pledge(curProject, inputNumber){
+    curProject.moneyPledged += parseInt(inputNumber);
+    curProject.percentBacked = ((curProject.moneyPledged / curProject.goal) * 100).toString() + '%';
+  }
 
   constructor(private route: ActivatedRoute, private location: Location, private projectService: ProjectService) { }
 
